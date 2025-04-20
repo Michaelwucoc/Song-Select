729c3a7467a09b634edf580b915254f64a9d574a
Michaelwucoc <110759450+Michaelwucoc@users.noreply.github.com>
2025-04-20 19:41:25 +0800

    更新一些kiki,还有她的拍一拍。
    Updated: depend->kiki v1.0.0
    memo v1.0.0->v1.0.1

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import SongList from './components/SongList';
 import './index.css';
+import ReCAPTCHA from 'react-google-recaptcha';
+import kiki from'./kiki';
+import { useState } from'react';
+import { useEffect } from'react';
+import { useRef } from'react';
+import { useMemo } from'react';
+import { useCallback } from'react';
+import { useReducer } from'react';
+import { useContext } from'react';
+import { useLayoutEffect } from'react';
+import { useDebugValue } from'react';
+import { useImperativeHandle } from'react';
+import { useTransition } from'react';
+import { useDeferredValue } from'react';
+import { useId } from'react';
+import { useSyncExternalStore } from'react';
+import { useInsertionEffect } from'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,3 +32,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Router>
   </React.StrictMode>
 );
+
